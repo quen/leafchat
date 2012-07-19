02f8df697807de2edf291ee816c3a62cd54e5436
sam marshall <sam@leafdigital.com>
2012-07-19 19:05:48 +0100

    Update copyright year on files touched this update

--- src/com/leafdigital/irc/IgnoreListSingleton.java
+++ src/com/leafdigital/irc/IgnoreListSingleton.java
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with leafChat. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright 2011 Samuel Marshall.
+Copyright 2012 Samuel Marshall.
 */
 package com.leafdigital.irc;
 
